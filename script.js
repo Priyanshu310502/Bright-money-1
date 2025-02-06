2057f53dcde237b808d91c53e3d6ece3cef16cca
Priyanshu310502 <kumawatpriyanshu993@gmail.com>
2025-02-06 11:21:10 +0530

    commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// // JavaScript for Navbar Behavior (Optional)
+// document.addEventListener("DOMContentLoaded", function () {
+//     const navbar = document.querySelector(".navbar");
+
+//     window.addEventListener("scroll", function () {
+//         if (window.scrollY > 50) {
+//             navbar.style.backgroundColor = "#e0ebea";
+//             navbar.style.transition = "0.3s";
+//         } else {
+//             navbar.style.backgroundColor = "#eef4ee";
+//         }
+//     });
+// });
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     const cards = document.querySelectorAll(".card");
+//     let currentIndex = 0;
+
+//     function showNextCard() {
+//         cards[currentIndex].classList.remove("active");
+//         currentIndex = (currentIndex + 1) % cards.length;
+//         cards[currentIndex].classList.add("active");
+//     }
+
+//     // Show the first card initially
+//     cards[currentIndex].classList.add("active");
+
+//     // Set interval for rotating testimonials
+//     setInterval(showNextCard, 5000); // Change testimonial every 5 seconds
+// });
+
+
+
+
+
